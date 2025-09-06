e3b88b3b98bfd5b46dfa04146fbe00a14b71a957
Shamima <shamima2802@gmail.com>
2025-09-06 20:44:52 +0530

    Initial deployment of Gemini Chat

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/chatbot/",
-  esbuild: {
-    jsx: 'react-jsx',
-  }
+  base: "/chatbot/",   // ✅ correct for GitHub Pages
 })
